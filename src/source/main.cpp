3560aa1e4c4c55724d2b9b2cddd3846d4aa9b7d1
Fuzznip <fuzznip@gmail.com>
2022-09-08 23:09:26 -0700

    Enabled sample shading for texture anti aliasing

--- src/source/main.cpp
+++ src/source/main.cpp
@@ -1424,9 +1424,9 @@ private:
 
     VkPipelineMultisampleStateCreateInfo multisampling{};
     multisampling.sType = VK_STRUCTURE_TYPE_PIPELINE_MULTISAMPLE_STATE_CREATE_INFO;
-    multisampling.sampleShadingEnable = VK_FALSE;
+    multisampling.sampleShadingEnable = VK_TRUE;
     multisampling.rasterizationSamples = msaaSamples;
-    multisampling.minSampleShading = 1.0f; // Optional
+    multisampling.minSampleShading = .2f; // Optional
     multisampling.pSampleMask = nullptr; // Optional
     multisampling.alphaToCoverageEnable = VK_FALSE; // Optional
     multisampling.alphaToOneEnable = VK_FALSE; // Optional
@@ -1712,6 +1712,7 @@ private:
 
     VkPhysicalDeviceFeatures deviceFeatures{};
     deviceFeatures.samplerAnisotropy = VK_TRUE;
+    deviceFeatures.sampleRateShading = VK_TRUE;
 
     VkDeviceCreateInfo createInfo{};
     createInfo.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
