c641ce824b5f4facfff7dceea5fe0c2088338950
Fuzznip <fuzznip@gmail.com>
2022-09-09 06:45:17 -0700

    Testing c++20 designated initializers

--- src/source/main.cpp
+++ src/source/main.cpp
@@ -1148,9 +1148,10 @@ private:
     renderPassInfo.renderArea.offset = { 0, 0 };
     renderPassInfo.renderArea.extent = swapChainExtent;
 
-    std::array<VkClearValue, 2> clearValues{};
-    clearValues[0].color = { { 0.0f, 0.0f, 0.0f, 1.0f } };
-    clearValues[1].depthStencil = { 1.f, 0 };
+    std::array<VkClearValue, 2> clearValues{
+      VkClearValue{ .color = {{ 0.0f, 0.0f, 0.0f, 1.0f }}},
+      VkClearValue{ .depthStencil = { 1.f, 0 }}
+    };
 
     renderPassInfo.clearValueCount = clearValues.size();
     renderPassInfo.pClearValues = clearValues.data();
